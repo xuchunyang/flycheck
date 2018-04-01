a1f6b5b7fd980547a6e8ef31ed54fd345b7a8d79
fmdkdd <fmdkdd@0xc0de.fr>
2018-04-01 17:32:18 +0200

    Install tools from NPM and GEM

---
+++ .travis/install_dependencies.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+gem install --no-document \
+    haml \
+    mdl \
+    puppet-lint \
+    reek \
+    rubocop \
+    ruby-lint \
+    sass \
+    scss_lint \
+    scss_lint_reporter_checkstyle \
+    slim \
+    sqlint \
+    foodcritic
+
+npm install --global \
+    coffeescript \
+    csslint \
+    eslint \
+    handlebars \
+    js-yaml
+    jscs \
+    jsonlint \
+    less \
+    markdownlint-cli \
+    pug-cli \
+    semistandard \
+    standard \
+    tslint \
+    typescript
